5db5d6b7ecdda0f3c71421ed89e42d75a04dac35
sandeep4102 <sandeep.sarswat@mail.vinove.com>
2020-09-17 20:58:42 +0530

    deploying on heroku on 17 sept

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const adminLoginRoute = require ('./modules/admin/admin-login/admin-login.routes')
 const registerProdRoute = require ('./modules/admin/admin.catalog/admin-catalog-routes')
+
+var cors = require ('cors')
 require('./databaseConfig/db');
 
 
@@ -11,6 +13,7 @@ var app = express();
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors())
 const port = process.env.PORT || 3001;  
 
 app.use("/admin", adminLoginRoute);
